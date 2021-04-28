e2ae5f2c604f5ad8c8bb829dbe5283c340f0f503
Karthik Shetty <f20180141@hyderabad.bits-pilani.ac.in>
2021-04-29 02:05:10 +0530

    Shopping Cart Done!

--- cypress/integration/cart.spec.js
+++ cypress/integration/cart.spec.js
@@ -1,9 +1,81 @@
+const addingCourse = () => {
+  let popup =
+    "div.course-details-quick-view-box--popover-wrapper--3jFIa > div > div > div > div";
+  cy.get("#course-unit-container-Studentsareviewing > [data-index = 3]").as(
+    "course"
+  );
+
+  cy.wait(5000);
+  cy.get("@course").trigger("mouseover");
+  cy.wait(2000);
+  cy.get("@course").trigger("mouseout");
+  cy.wait(5000);
+  cy.get("@course").trigger("mouseover");
+  cy.get("[data-purpose=add-to-cart]")
+    .should("exist")
+    .and("be.visible")
+    .click();
+
+  cy.get("[data-purpose = close-popup]").click();
+
+  //Check cart now should have 1 item
+  cy.get("[data-purpose = cart-icon]").trigger("mouseover");
+  cy.get("a.panel-menu--item--3U1oy").should("exist").and("have.length", 1);
+
+  cy.get("[data-purpose=header-shopping-cta]")
+    .should("exist")
+    .and("be.visible")
+    .and("have.attr", "href", "/cart/")
+    .and("have.text", "Go to cart")
+    .click();
+};
+
 describe("Checking the Cart functionality", () => {
   beforeEach(() => {
     cy.VisitAndWait();
   });
 
   context("Desktop View", () => {
-    
+    it("Empty Cart", () => {
+      cy.get("[data-purpose=header-shopping-cta]").should("not.exist");
+      cy.get("[data-purpose = cart-icon]").trigger("mouseover");
+      cy.get("[data-purpose=header-shopping-cta]")
+        .should("exist")
+        .and("be.visible")
+        .and("have.attr", "href", "/")
+        .and("have.text", "Keep shopping")
+        .click();
+    });
+
+    it("Adding a course", () => {
+      //Cart should not have any items initially
+      cy.get("div.shopping-item--buyable-info--1k-48").should("not.exist");
+      addingCourse();
+    });
+
+    it("Deleting a course", () => {
+      addingCourse();
+
+      cy.url().should("eq", "https://www.udemy.com/cart/");
+
+      cy.get("[data-purpose = cart-list-title]")
+        .should("exist")
+        .and("be.visible")
+        .and("have.text", "1 Course in Cart");
+
+      cy.get("[data-purpose = actions]").click();
+
+      cy.get("[data-purpose = cart-list-title]")
+        .should("exist")
+        .and("be.visible")
+        .and("have.text", "0 Courses in Cart");
+
+      cy.get("[data-purpose = keep-shopping-action]")
+        .should("exist")
+        .and("be.visible")
+        .click();
+
+      cy.url().should("eq", "https://www.udemy.com/");
+    });
   });
 });
--- cypress/integration/trending-previews.spec.js
+++ cypress/integration/trending-previews.spec.js
@@ -17,13 +17,57 @@ describe("Intercept Requests on 'Students are Viewing' Panel", () => {
         "div.course-details-quick-view-box--popover-wrapper--3jFIa > div > div > div > div";
 
       for (let i = 0; i < 18; i++) {
-        if (i != 0) cy.get(popup).should("not.exist");
+        if (i != 0) {
+          cy.get(popup).should("not.exist");
+        }
+
         cy.wait(2000);
         cy.get(`@course${i}`).trigger("mouseover");
         cy.wait(2000);
-        if (i != 0) cy.get(popup).should("exist").and("be.visible");
+
+        if (i != 0) {
+          cy.get(popup).should("exist").and("be.visible");
+
+          //Card Components Testing
+          cy.get(popup).within(() => {
+            cy.get("[data-purpose = quick-view-box-title]")
+              .should("exist")
+              .and("be.visible");
+            cy.get("div.course-details-quick-view-box--badge-container--1Qfpa")
+              .should("exist")
+              .and("be.visible");
+            cy.get("span.course-details-quick-view-box--updated--2bmvS")
+              .should("exist")
+              .and("be.visible")
+              .contains("Updated");
+            cy.get("div.course-details-quick-view-box--stats--3pBYQ")
+              .should("exist")
+              .and("be.visible");
+            cy.get("div.course-details-quick-view-box--headline--GZKCw")
+              .should("exist")
+              .and("be.visible");
+            cy.get("div.course-details-quick-view-box--objectives--3WCDQ")
+              .should("exist")
+              .and("be.visible");
+            cy.get("[data-purpose= quick-view-box-objective]")
+              .should("exist")
+              .and("be.visible")
+              .and("have.length", 3);
+            cy.get("[data-purpose=add-to-cart]")
+              .should("exist")
+              .and("be.visible");
+            cy.get("[data-purpose= toggle-wishlist]")
+              .should("exist")
+              .and("be.visible");
+          });
+        }
+
         cy.get(`@course${i}`).trigger("mouseout");
         cy.wait(2000);
+
+        if (i != 0) {
+          cy.get(popup).should("not.exist");
+        }
 
         if (i != 0 && i % 3 == 0) {
           cy.get(
